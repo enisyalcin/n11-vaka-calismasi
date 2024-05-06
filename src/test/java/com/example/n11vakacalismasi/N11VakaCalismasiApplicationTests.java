3aa1434a6e83d49262330189ab0cd2172585d287
enis.yalcin <enis.yalcin@tubitak.gov.tr>
2024-05-06 14:09:36 +0300

    Add Spring project files

---
+++ src/test/java/com/example/n11vakacalismasi/N11VakaCalismasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.n11vakacalismasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11VakaCalismasiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
